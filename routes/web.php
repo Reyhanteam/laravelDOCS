2688f490a04a845696faea66f360fed04d6963c1
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-09 22:01:30 +0330

    feat(docs): route documentation through Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\DocumentationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DocumentationController::class, 'home'])->name('home');
+Route::get('/docs/search', [DocumentationController::class, 'search'])->name('docs.search');
+Route::get('/docs/{path?}', [DocumentationController::class, 'show'])
+    ->where('path', '.*')
+    ->name('docs.show');
